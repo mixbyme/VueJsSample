179e88d10e7cb97d8bf3a906fdb218fcb3ef96cf
yongho.lee <yongho.lee@medit.com>
2019-09-10 13:06:00 +0900

    init

---
+++ src/app/app/Error/Error.vue
@@ -0,0 +1,50 @@
+<template>
+  <div :class="$style.error">
+    <vue-icon-vuesion :class="$style.logo" />
+    <vue-headline level="1">Error</vue-headline>
+    <vue-headline level="3">Try again later!</vue-headline>
+  </div>
+</template>
+
+<script lang="ts">
+import VueHeadline from '@components/VueHeadline/VueHeadline.vue';
+import VueIconVuesion from '@components/icons/VueIconVuesion/VueIconVuesion.vue';
+
+export default {
+  metaInfo: {
+    title: 'vuesion - an error occurred',
+    meta: [
+      {
+        name: 'robots',
+        content: 'NOINDEX,NOFOLLOW',
+      },
+    ],
+  },
+  name: 'Error',
+  components: { VueIconVuesion, VueHeadline },
+  props: {},
+  data(): any {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" module>
+@import '~@/app/shared/design-system';
+
+.error {
+  padding-top: $nav-bar-height;
+  text-align: center;
+  width: 300px;
+  height: 300px;
+  margin: auto;
+  margin-top: 25%;
+}
+
+.logo {
+  width: $space-84;
+  height: $space-84;
+}
+</style>
---
+++ src/app/counter/Counter/Counter.spec.ts
@@ -0,0 +1,78 @@
+import { createLocalVue, mount } from '@vue/test-utils';
+import Vuex, { ActionTree, GetterTree, Store } from 'vuex';
+import Counter from './Counter.vue';
+import { CounterGetters, ICounterGetters } from '../getters';
+import { CounterDefaultState, ICounterState } from '../state';
+import { CounterActions, ICounterActions } from '../actions';
+import { i18n } from '@shared/plugins/i18n/i18n';
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
+describe('Counter.vue', () => {
+  let store: Store<ICounterState>;
+  let getters: GetterTree<ICounterState, ICounterGetters>;
+  let actions: ActionTree<ICounterState, ICounterActions>;
+  let state: ICounterState;
+
+  beforeEach(() => {
+    getters = {
+      ...CounterGetters,
+    };
+    actions = {
+      ...CounterActions,
+      increment: jest.fn(),
+      decrement: jest.fn(),
+    };
+    state = {
+      ...CounterDefaultState(),
+    };
+
+    store = new Vuex.Store({
+      modules: {
+        counter: {
+          namespaced: true,
+          getters,
+          actions,
+          state,
+        },
+      },
+    } as any);
+  });
+
+  test('renders component', () => {
+    const wrapper = mount<any>(Counter, {
+      store,
+      localVue,
+      i18n,
+      stubs: ['router-link'],
+    });
+
+    expect(wrapper.find('h1').text()).toBe('Counter');
+  });
+
+  test('should increment and decrement', () => {
+    const wrapper = mount<any>(Counter, {
+      store,
+      localVue,
+      i18n,
+      stubs: ['router-link'],
+    });
+
+    wrapper.vm.increment();
+    expect(actions.increment).toHaveBeenCalled();
+
+    wrapper.vm.decrement();
+    expect(actions.decrement).toHaveBeenCalled();
+  });
+
+  test('dispatches action on the server', () => {
+    store.dispatch = jest.fn();
+
+    Counter.prefetch({ store });
+
+    expect(store.dispatch).toHaveBeenCalled();
+    expect(store.dispatch).toHaveBeenCalledWith(`counter/increment`);
+  });
+});
